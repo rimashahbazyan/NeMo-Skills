9ff2343e5d71047d6d1f5205e97d57c2d1547e50
Rima Shahbazyan <rshahbazyan@nvidia.com>
2025-08-27 11:36:32 +0400

    error type generation added

--- nemo_skills/inference/factory.py
+++ nemo_skills/inference/factory.py
@@ -5,10 +5,12 @@ class GenerationType(str, Enum):
     generate = "generate"
     math_judge = "math_judge"
     check_contamination = "check_contamination"
+    stem_error_type = "stem_error_type"
 
 
 GENERATION_MODULE_MAP = {
     GenerationType.generate: "nemo_skills.inference.generate",
     GenerationType.math_judge: "nemo_skills.inference.llm_math_judge",
     GenerationType.check_contamination: "nemo_skills.inference.check_contamination",
+    GenerationType.stem_error_type: "nemo_skills.inference.stem_error_type",
 }
